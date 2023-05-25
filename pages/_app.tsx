49e7814e8e8a0a8273cd19abf2ccea9c68d7617a
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-05-24 22:19:13 -0500

    feat: setup initial next-auth signin with Github

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { SessionProvider, SessionProviderProps } from "next-auth/react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps }
+}: AppProps<SessionProviderProps>) {
+  return (
+    <SessionProvider>
+      <Component {...pageProps} />;
+    </SessionProvider>
+  );
 }
